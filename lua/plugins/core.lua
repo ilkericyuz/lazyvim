f1eb35bf7dbe17914644e0b578357523278db5f1
ilker <ilker@demets-MacBook-Pro.local>
2024-09-29 08:59:32 +0300

    feat: catppuccin with configuration

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,6 +1,60 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin-macchiato",
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    -- you can do it like this with a config function
+    config = function()
+      require("catppuccin").setup({
+        -- configurations
+      })
+    end,
+    -- or just use opts table
+    opts = {
+      -- configurations
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
   },
 }
