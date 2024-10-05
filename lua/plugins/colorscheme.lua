7c826e72f96ff5a362580f77a4b541a5411a3b0d
ilker icyuz <ilker.icyuz@bitpanda.com>
2024-10-05 10:31:36 +0200

    feat: add colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,199 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = true,
+    {
+      "tiagovla/tokyodark.nvim",
+      opts = {
+        -- custom options here
+      },
+      config = function(_, opts)
+        require("tokyodark").setup(opts) -- calling setup is optional
+        -- vim.cmd([[colorscheme tokyodark]])
+      end,
+    },
+
+    lazy = false,
+    priority = 1000,
   },
 
   {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
+    "BeardedBear/bearded-theme",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "killitar/obscure.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("obscure").setup({
+        styles = {
+          booleans = { italic = true, bold = true },
+        },
+        on_highlights = function(hl, c)
+          hl.Comment = { fg = "#ffffff" }
+          -- Or
+          hl.Comment = { fg = c.orange }
+        end,
+      })
+    end,
+  },
+
+  {
+    "cdmill/neomodern.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("neomodern").setup({
+        -- Main options --
+        -- style = "iceclimber", -- choose between 'iceclimber', 'coffeecat', 'darkforest', 'campfire', 'roseprime', 'daylight'
+        toggle_style_key = nil, -- keymap to cycle between styles
+        toggle_style_list = { -- a table of which styles to cycle through, by default all styles are included
+          "iceclimber",
+          "coffeecat",
+          "darkforest",
+          -- "campfire",
+          "roseprime",
+          "daylight",
+        },
+        transparent = false, -- don't set background
+        term_colors = true, -- if true enable the terminal
+
+        -- Formatting --
+        code_style = {
+          comments = "italic",
+          conditionals = "none",
+          functions = "none",
+          keywords = "none",
+          headings = "bold", -- markdown headings
+          operators = "none",
+          keyword_return = "none",
+          strings = "none",
+          variables = "none",
+        },
+
+        -- UI options --
+        ui = {
+          colored_docstrings = true, -- if true, docstrings will be highlighted like strings, otherwise they will be highlighted like comments
+          plain_float = false, -- don't set background of floating windows. recommended for when using floating windows with borders
+          show_eob = true, -- show the end-of-buffer tildes
+
+          -- Plugins Related --
+          lualine = {
+            bold = true,
+            plain = false, -- use a less distracting lualine. note: works best when no lualine separators are used
+          },
+          cmp = {
+            plain = false, -- don't highlight lsp-kind items
+            reverse = false, -- reverse item kind highlights in cmp menu
+          },
+          telescope = "borderless", -- choose between 'borderless' or 'bordered'
+          diagnostics = {
+            darker = true, -- darker colors for diagnostic
+            undercurl = true, -- use undercurl for diagnostics
+            background = true, -- use background color for virtual text
+          },
+        },
+
+        -- Custom Highlights --
+        colors = {}, -- Override default colors
+        highlights = {}, -- Override highlight groups
+      })
+    end,
+  },
+
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      -- vim.cmd("colorscheme github_dark")
+    end,
+  },
+
+  {
+    "wojciechkepka/vim-github-dark",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "rockyzhang24/arctic.nvim",
+    branch = "v2",
+    dependencies = { "rktjmp/lush.nvim" },
+    priority = 1000,
+  },
+
+  {
+    "sharpchen/Eva-Theme.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").setup({
+        bold_keywords = false,
+        -- Enable italic comments.
+        italic_comments = true,
+        -- Enable editor background transparency.
+        transparent = {
+          -- Enable transparent background.
+          bg = false,
+          -- Enable transparent background for floating windows.
+          float = false,
+        },
+        -- Enable brighter float border.
+        bright_border = true,
+        -- Reduce the overall amount of blue in the theme (diverges from base Nord).
+        reduced_blue = true,
+        -- Swap the dark background with the normal one.
+        swap_backgrounds = false,
+        -- Cursorline options.  Also includes visual/selection.
+        cursorline = {
+          -- Bold font in cursorline.
+          bold = false,
+          -- Bold cursorline number.
+          bold_number = true,
+          -- Available styles: 'dark', 'light'.
+          theme = "light",
+          -- Blending the cursorline bg with the buffer bg.
+          blend = 0.85,
+        },
+        noice = {
+          -- Available styles: `classic`, `flat`.
+          style = "classic",
+        },
+        telescope = {
+          -- Available styles: `classic`, `flat`.
+          style = "classic",
+        },
+        leap = {
+          -- Dims the backdrop when using leap.
+          dim_backdrop = false,
+        },
+        ts_context = {
+          -- Enables dark background for treesitter-context window
+          dark_background = true,
+        },
+      })
+    end,
   },
 
   {
     "catppuccin/nvim",
-    lazy = true,
+    lazy = false,
+    priority = 1000,
     name = "catppuccin",
     -- you can do it like this with a config function
     -- config = function()
@@ -64,7 +246,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "github_dark",
     },
   },
 }
