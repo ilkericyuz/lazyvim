a9a62389a28157701fb5148b1d4c0064b2eb65b0
ilker icyuz <ilker.icyuz@bitpanda.com>
2024-10-10 09:18:36 +0200

    feat: add fast move keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume last telescope session" }
 )
+
+vim.keymap.set("n", "<C-S-j>", "5j", { desc = "Move up fast" })
+vim.keymap.set("n", "<C-S-k>", "5k", { desc = "Move up fast" })
+vim.keymap.set("n", "<C-S-h>", "8h", { desc = "Move left fast" })
+vim.keymap.set("n", "<C-S-l>", "8l", { desc = "Move right fast" })
